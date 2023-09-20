52bf2b7defbf43f4c314f3415d134a8d3ffecb78
Matwey Ivanov <matvey.ivanov@medeum.ru>
2023-09-20 12:19:44 +0000

    Add Forest analyzer service

---
+++ src/services/forest_analyzer.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+from src.services.forest_classifier import ForestClassifier
+
+
+class ForestAnalyzer:
+    def __init__(self, classes_list: list[str], thresholds_list: list[float], classifier: ForestClassifier) -> None:
+        """Inits Forest analyzer service
+
+        Args:
+            classes_list (list[str]): class names list
+            thresholds_list (list[float]): list of thresholds for each tag
+            classifier (ForestClassifier): classifier object for making predictions
+        """
+        self._class_names_list = classes_list
+        self._thresholds = np.array(thresholds_list)
+        self._classifier = classifier
+
+    def predict(self, image: np.ndarray) -> list[str]:
+        """Returns tag names for the specified image
+
+        Args:
+            image (np.ndarray): input image in BGR format
+
+        Returns:
+            list[str]: list of tags
+        """
+        raw_prediction = self._classifier.predict(image)
+        filtered_pred_arr = np.where(raw_prediction >= self._thresholds)[0]
+
+        return [self._class_names_list[class_ind] for class_ind in filtered_pred_arr]
+
+    def predict_proba(self, image: np.ndarray) -> dict[str, float]:
+        """Predicts probabilities of each tag for this image
+
+        Args:
+            image (np.ndarray): input image in BGR format
+
+        Returns:
+            dict[str, float]: dictionary of {tag: score} items
+        """
+        raw_prediction = self._classifier.predict(image)
+
+        return {self._class_names_list[class_ind]: class_prob for class_ind, class_prob in enumerate(raw_prediction)}
